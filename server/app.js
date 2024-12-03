2576888541249c32757f2080fdf4f0a8e40b0c5b
Emilia Junghanns <emijunghanns@hotmail.com>
2024-12-03 17:05:27 +0100

    day2

--- server/app.js
+++ server/app.js
@@ -5,13 +5,21 @@ const cohorts = require("./cohorts.json");
 const students = require("./students.json");
 const PORT = 5005;
 const cors = require("cors");
-
+const mongoose = require("mongoose");
+const Cohort = require("./models/Cohort.model");
+const Student = require("./models/Student.model");
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
+
+//DATABASE CONNECTION
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -25,18 +33,64 @@ const settings = { origin: [`http://localhost:5005`, `http://localhost:5173`] };
 app.use(cors(settings));
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
+
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+//GET COHORTS ROUTES
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
+  Cohort.find({})
+    .then((cohorts) => {
+      console.log("Retrieved cohorts -->", cohorts);
+      res.json(cohorts);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving cohorts -->", error);
+      res.status(500).json({ error: "Failed to retrieve cohorts" });
+    });
 });
 
+app.get("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.find({ _id: "616c4b4c649eaa001dd50f81" })
+    .then((cohorts) => {
+      console.log("Retrieved cohorts -->", cohorts);
+      res.json(cohorts);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving cohorts -->", error);
+      res.status(500).json({ error: "Failed to retrieve cohorts" });
+    });
+});
+
+//POST COHORTS ROUTES
+
+//GET STUDENTS ROUTES
+
 app.get("/api/students", (req, res) => {
-  res.json(students);
+  Student.find({})
+    .then((students) => {
+      console.log("Retrieved students -->", students);
+      res.json(students);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving students -->", error);
+      res.status(500).json({ error: "Failed to retrieve students" });
+    });
+});
+
+app.get("/api/students/:studentId", (req, res) => {
+  Student.find({ _id: "616c4b4c649eaa001dd50f85" })
+    .then((students) => {
+      console.log("Retrieved students -->", students);
+      res.json(students);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving students -->", error);
+      res.status(500).json({ error: "Failed to retrieve students" });
+    });
 });
 
 // START SERVER
---
+++ server/models/Student.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// CREATE SCHEMA
+// Schema - describes and enforces the structure of the documents
+const studentSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  linkedinUrl: String,
+  languages: {
+    type: String,
+    enum: [
+      "English",
+      "Spanish",
+      "French",
+      "German",
+      "Portuguese",
+      "Dutch",
+      "Other",
+    ],
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  background: String,
+  image: { type: String, defalut: "https://i.imgur.com/r8bo8u7.png" },
+  cohort: Number,
+  projects: Array,
+});
+
+// CREATE MODEL
+// The model() method defines a model (Cohort) and creates a collection (cohorts) in MongoDB
+// The collection name will default to the lowercased, plural form of the model name:
+
+const Student = mongoose.model("Student", studentSchema);
+
+// EXPORT THE MODEL
+module.exports = Student;
